60de8a1d2813679b767d5bdb45f96ecb761407cb
Manish Gupta <mk.kumar666888@gmail.com>
2025-04-24 13:52:40 +0530

    fix Error

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -49,6 +49,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaderes.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware'
 ]
 
 ROOT_URLCONF = 'backend.urls'
